c3844790cb061e5a0281b4cdc2d98bb5e94772ec
Pablo <147501769+Pablosky76@users.noreply.github.com>
2026-03-23 20:42:33 +0100

    Añade tests Cypress de login y shifting content

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,23 +2,40 @@ describe('Test Login - Form Authentication', () => {
 
   it('Debe loguearse correctamente', () => {
 
-    // Ir a la página
+    // 1. Ir a la página
     cy.visit('https://the-internet.herokuapp.com/')
 
-    // Clicar en "Form Authentication"
+    // 2. Clicar en "Form Authentication"
     cy.contains('Form Authentication').click()
 
-    // Rellenar datos
+    // 3. Rellenar datos
     cy.get('#username').type('tomsmith')
     cy.get('#password').type('SuperSecretPassword!')
 
-    // Clicar en Login
+    // 4. Clicar en Login
     cy.get('button[type="submit"]').click()
 
-    // Comprobar mensaje de bienvenida
+    // 5. Comprobar mensaje de bienvenida
     cy.get('#flash')
       .should('be.visible')
       .and('contain', 'You logged into a secure area!')
+
+  })
+
+  it('Debe comprobar que el menú tiene 5 elementos en Shifting Content', () => {
+
+    // 1. Entrar en la página
+    cy.visit('https://the-internet.herokuapp.com/')
+
+    // 2. Ir a "Shifting Content"
+    cy.contains('Shifting Content').click()
+
+    // 3. Ir a "Example 1: Menu Element"
+    cy.contains('Example 1: Menu Element').click()
+
+    // 4. Comprobar que el menú tiene 5 elementos
+    cy.get('.example ul li')
+      .should('have.length', 5)
 
   })
 
